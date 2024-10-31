b0be98300a38cb960240241f9c792bec3dd8a7f4
Rorisang Putu <rorisangputu@gmail.com>
2024-11-01 00:21:14 +0200

    creating animations files to call animations from

---
+++ app/components/Loader/animations.js
@@ -0,0 +1,22 @@
+export const introAnimation = (wordsGroupRef) => {
+  const tl = gsap.timeline();
+
+  tl.to(wordsGroupRef.current, {
+    yPercent: -80,
+    duration: 5,
+    ease: "power3.inOut",
+  });
+
+  return tl;
+};
+
+export const progressAnimation = (progressRef) => {
+  const tl = gsap.timeline();
+  //Progress bar animation controll
+  tl.to(progressRef.current, {
+    scaleX: 1,
+    duration: 5,
+    ease: "power3.inOut",
+  });
+  return tl;
+};
